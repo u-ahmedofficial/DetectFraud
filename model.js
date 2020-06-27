a19e254edffbfe55761cfb18f32654023deb2e28
u-ahmedofficial <umairahmedofficial1@gmail.com>
2020-06-28 04:18:53 +0500

    Add Complete Schema

--- model.js
+++ model.js
@@ -69,14 +69,102 @@ function createDB()
 		conn.query('CREATE TABLE Payments(Gender integer,Married integer,Dependents integer,Education integer,Self_Employed integer,ApplicantIncome float,Coapplicant_Income float,LoanAmount float,Loan_Amount_Term float,CreditHistory float,Property_Area integer)',(error,result)=>
 			{
 				if (error){ 
-					console.log("Error Creating Payments Table!");
-				}
+					console.log("    ");
+				}else{
+
+						const fs = require('fs');
+						const csv = require('fast-csv');
+						let stream = fs.createReadStream("ULoan_Test");
+						let myData = [];
+						let csvStream = csv
+									.parse()
+									.on("data", function (data) {
+											myData.push(data);
+																})
+									.on("end", function () {
+													myData.shift();
+		
+									// create a new connection to the database
+									const connection = mysql.createConnection({
+											host: 'localhost',
+											user: 'root',
+											password: '',
+											database: 'detectfraud'
+																});
+
+									// open the connection
+									connection.connect((error) => {
+									if (error) {
+										console.error(error);
+										} else {
+										let query = 'INSERT INTO Payments(Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,Coapplicant_Income,LoanAmount,Loan_Amount_Term,CreditHistory,Property_Area) VALUES ?';
+										connection.query(query, [myData], (error, response) => {
+										if(error) console.log("Error Inserting Records to Payments!");
+																	});
+												}
+																});
+														});
+
+										stream.pipe(csvStream);
+					}
+
 			});
-		conn.query('CREATE TABLE Fraud(Gender integer,Married integer,Dependents integer,Education integer,Self_Employed integer,ApplicantIncome float,Coapplicant_Income float,LoanAmount float,Loan_Amount_Term float,CreditHistory float,Property_Area integer)',(error,result)=>
+
+		conn.query('CREATE TABLE EXISTS Fraud(Gender integer,Married integer,Dependents integer,Education integer,Self_Employed integer,ApplicantIncome float,Coapplicant_Income float,LoanAmount float,Loan_Amount_Term float,CreditHistory float,Property_Area integer)',(error,result)=>
 			{
 				if (error){ 
-					console.log("Error Creating Fraud Table!");
-				}
+					console.log(" ");
+				}else {
+					const {spawn} = require('child_process');
+					var dataToSend;
+					// spawn new child process to call the python script
+					const python = spawn('python3', ['UM.py']);
+					// collect data from script
+					python.stdout.on('data', function (data) {
+					console.log('Pipe data from python script ...');
+					dataToSend = data.toString();
+															});
+					// in close event we are sure that stream from child process is closed
+					python.on('close', (code) => {
+					//console.log(`child process close all stdio with code ${code}`);
+					// send data to browser
+					const fs = require('fs');
+						const csv = require('fast-csv');
+						let stream = fs.createReadStream("fraud");
+						let myData = [];
+						let csvStream = csv
+									.parse()
+									.on("data", function (data) {
+											myData.push(data);
+																})
+									.on("end", function () {
+													myData.shift();
+		
+									// create a new connection to the database
+									const conn = mysql.createConnection({
+											host: 'localhost',
+											user: 'root',
+											password: '',
+											database: 'detectfraud'
+																});
+
+									// open the connection
+									conn.connect((error) => {
+									if (error) {
+										console.error(error);
+										} else {
+										let query = 'INSERT INTO Fraud(Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,Coapplicant_Income,LoanAmount,Loan_Amount_Term,CreditHistory,Property_Area) VALUES ?';
+										conn.query(query, [myData], (error, response) => {
+										if(error) console.log("Error Inserting Records to Fraud!");
+																	});
+												}
+																});
+														});
+
+										stream.pipe(csvStream);
+					
+												});
+				}	
 			});
 		
 		
