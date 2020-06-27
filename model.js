7af557b6f3d7da06713ad164bd54ad4294a5bc7d
u-ahmedofficial <umairahmedofficial1@gmail.com>
2020-06-28 02:42:46 +0500

    Add All Tables

--- model.js
+++ model.js
@@ -52,26 +52,38 @@ function createDB()
 		else
 		{
 
-		conn.query('CREATE TABLE IF NOT EXISTS Users(Id integer AUTO_INCREMENT, Role integer, Username varchar(255), Email varchar(255), password varchar(255), PRIMARY KEY(Id,Email,Username))',(error,result)=>
+		conn.query('CREATE TABLE IF NOT EXISTS Users(Id integer AUTO_INCREMENT, Role integer NOT NULL, Username varchar(255) NOT NULL UNIQUE, Email varchar(255) UNIQUE, Password varchar(255) NOT NULL, PRIMARY KEY(Id,Email))',(error,result)=>
 			{
 				if (error){ 
-					console.log("Error Creating Users Table!");
+					console.log(error);
 				}
 			});
 
-		conn.query('CREATE TABLE IF NOT EXISTS Sessions(username varchar(255), Role integer, Active integer, PRIMARY KEY(username))',(error,result)=>
+		conn.query('CREATE TABLE IF NOT EXISTS Sessions(username varchar(255), Role integer NOT NULL, Active integer NOT NULL, PRIMARY KEY(username))',(error,result)=>
 			{
 				if (error){ 
 					console.log("Error Creating Sessions Table!");
 				}
 			});
 
-
+		conn.query('CREATE TABLE Payments(Gender integer,Married integer,Dependents integer,Education integer,Self_Employed integer,ApplicantIncome float,Coapplicant_Income float,LoanAmount float,Loan_Amount_Term float,CreditHistory float,Property_Area integer)',(error,result)=>
+			{
+				if (error){ 
+					console.log("Error Creating Payments Table!");
+				}
+			});
+		conn.query('CREATE TABLE Fraud(Gender integer,Married integer,Dependents integer,Education integer,Self_Employed integer,ApplicantIncome float,Coapplicant_Income float,LoanAmount float,Loan_Amount_Term float,CreditHistory float,Property_Area integer)',(error,result)=>
+			{
+				if (error){ 
+					console.log("Error Creating Fraud Table!");
+				}
+			});
+		
 		
 		}
 	}); 
 
-	},20)
+	},30)
 
 
 }
